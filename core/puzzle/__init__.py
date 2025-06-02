3216f5c71428d344452bb5c287baec058cae54a0
BuildTools <unconfigured@null.spigotmc.org>
2025-06-02 16:36:25 +0200

    Correction d'un problème d'importation

--- core/puzzle/__init__.py
+++ core/puzzle/__init__.py
@@ -8,5 +8,3 @@ Created on Wed Aug  7 17:49:06 2024
 
 from core.puzzle.puzzle import Puzzle
 from core.puzzle.puzzle_solver import PuzzleSolver
-from core.puzzle.post_solver import PostSolver
-from core.puzzle.global_pattern_matching_solver import GPMSolver
